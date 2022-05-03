52beb63682a76a2e26468ce6f19d5a7c9b10b090
FallenHell <gabriel.ferreri98@gmail.com>
2022-05-03 18:09:18 -0500

    adding binary tree

--- Binary Tree Algorithm/BinnaryTree.js
+++ Binary Tree Algorithm/BinnaryTree.js
@@ -7,17 +7,90 @@ class Node {
 }
 
 class BinarySearchTree {
-    constructor(){
+    constructor() {
         this.root = null;
     }
-    insert(value){
+    insert(value) {
+        const node = new Node(value);
+        if (!this.root) {
+            this.root = node;
+            return this;
+        }
 
-    }
-    find(value){
+        let tree = this.root;
+        while (true) {
+            if (value < tree.value) {
+                if (!tree.left) {
+                    tree.left = node;
+                    return this;
+                }
 
+                tree = tree.left;
+            } else {
+                if (!tree.right) {
+                    tree.right = node;
+                    return this;
+                }
+                tree = tree.right
+            }
+        }
     }
-    remove(value){
-        
+    find(value) {
+        if (!this.root) return false;
+        let tree = this.root;
+        while (tree) {
+            if (value < tree.value) tree = tree.left;
+            else if (value > tree.value) tree = tree.right
+            else if (value === tree.value) return tree;
+        }
+
+        return false;
+    }
+    remove(value, current, parent) {
+        if (!this.root) return false;
+        while (current) {
+            if (value < current.value) {
+                parent = current;
+                current = current.left;
+            } else if (value > current.value) {
+                parent = current;
+                current = current.right;
+            } else {
+                if (current.left !== null & current.right !== null) {
+                    current.value = this.getMin(current.right);
+                    this.remove(current.value, current.right, current);
+                } else if (parent === null) {
+                    if (current.left !== null) {
+                        current.value = current.left.value;
+                        current.left = current.left.left;
+                        current.right = current.left.right;
+                    } else if (current.right !== null) {
+                        current.value = current.right.value;
+                        current.left = current.right.left;
+                        current.right = current.right.right;
+                    } else {
+                        this.root = null;
+                    }
+                } else if (current === parent.left) {
+                    parent.left = current.left !== null ?  current.left:current.right;
+                } else if (current === parent.right) {
+                    parent.right = current.right !== null? current.left : current.right;
+                }
+
+            }
+            break;
+        }
+        return this;
+    }
+
+    getMin(node) {
+        while (node.left !== null) {
+            node = node.left;
+        }
+        return node.value;
     }
 }
 
+
+
+let bst = new BinarySearchTree();
