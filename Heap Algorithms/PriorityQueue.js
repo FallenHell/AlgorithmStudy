abae45c6bb3954d42f67121ee9974c0651165e36
FallenHell <gabriel.ferreri98@gmail.com>
2022-05-11 01:31:33 -0500

    inserting priority queue and binary heaps

---
+++ Heap Algorithms/PriorityQueue.js
@@ -0,0 +1,74 @@
+class Node{
+    constructor(value, priority){
+        this.value = value;
+        this.priority = priority;
+    }
+}
+
+class PriorityQueue{
+    constructor(){
+        this.data = [];
+    }
+
+    enqueue(value, priority){
+        let node = new Node(value, priority);
+        this.data.push(node);
+        this.bubbleUp();
+        return this;
+    }
+
+    bubbleUp(){
+        let idx = this.data.length - 1;
+        const element = this.data[idx];
+        while(idx > 0) {
+            let parentIdx = Math.floor((idx - 1)/2);
+            let parent = this.data[parentIdx];
+            if(element.priority >= parent.priority) break;
+            this.data[idx] = parent;
+            this.data[parentIdx] = element; 
+            idx = parentIdx;
+        }
+    }
+
+    dequeue(){
+        const min = this.data[0];
+        const last = this.data.pop();
+        if(this.data.length > 0){
+            this.data[0] = last;
+            this.bubbleDown();
+        }
+        return min;
+    }
+
+    bubbleDown(){
+        let idx = 0;
+        let length = this.data.length;
+        let element = this.data[0];
+
+        while(true){
+            let leftChildIdx = 2*idx+1;
+            let rightChildIdx = 2*idx+2;
+            let leftChild, rightChild;
+            let smallest = null;
+            if(leftChildIdx < length) {
+                leftChild = this.data[leftChildIdx];
+                if (leftChild.priority < element.priority){
+                    smallest = leftChildIdx;
+                }
+            }
+
+            if(rightChildIdx < length){
+                rightChild = this.data[rightChildIdx];
+                if ((smallest === null && rightChild.priority < element.priority) || 
+                (smallest !== null && rightChild.priority < leftChild.priority)){
+                    smallest = rightChildIdx;
+                }
+            }
+            if (smallest === null) break;
+            this.data[idx] = this.data[smallest];
+            this.data[smallest] = element;
+            idx = smallest;
+        }
+    }
+}
+
